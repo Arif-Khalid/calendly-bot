b0fce7d8b099a7c2ed463258596b690e5fa9653a
arif <arifkhalid99@gmail.com>
2025-07-16 15:04:20 +0800

    Complete booking

--- constants.py
+++ constants.py
@@ -1,14 +1,27 @@
+# Does not perform final submission if true. Set to true when testing, set to False when using in production
+DEV_MODE=False                   
+
 # Bot variables
-LOAD_TIME_SECONDS=10
+LOAD_TIME_SECONDS=10            # Main page load time
+SHORT_LOAD_TIME_SECONDS=3       # How long to wait for things to become enabled after actions, should be adjusted to as short as possible for fast refreshes
 TIME_BETWEEN_REFRESH_SECONDS=1
+TIME_WAIT_AFTER_CLICK=1
+USER_DATA_DIR="--user-data-dir=/home/arif/.config/google-chrome/Default"
 
 # Booking variables
-DAY=14
+TIME="15:00"                    # Input in string of "HH:MM" in 24HR format. I.e. "14:00"
+DAY=16
 MONTH=7
 YEAR=2025
 BASE_URL="https://calendly.com/tabletennis-smrt/smrt-tabletennisbooking"
-EMAIL="example@gmail.com"
+NAME="nicholas"
+EMAIL="arifkhalid99@gmail.com"
 
+# Website variables
 # To be left alone unless website changes
 MONTH_STRING=str(YEAR) + '-' + str(MONTH).rjust(2, "0")
 URL=f"{BASE_URL}?month={MONTH_STRING}"
+LOADING_ELEMENT_XPATH="//div[@data-testid='calendar-loader']"
+TIME_BUTTON_XPATH=f"//button[@data-start-time='{TIME}']"
+NEXT_BUTTON_XPATH=f"//button[@aria-label='Next {TIME}']"
+SUBMIT_BUTTON_XPATH="//button[@type='submit']"
--- main.py
+++ main.py
@@ -1,49 +1,114 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 import time
 
-from constants import BASE_URL, DAY, MONTH, MONTH_STRING, TIME_BETWEEN_REFRESH_SECONDS, URL, LOAD_TIME_SECONDS, YEAR
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
 
-print("Hello World")
-driver = webdriver.Chrome()
-driver.implicitly_wait(LOAD_TIME_SECONDS)
+from constants import BASE_URL, DAY, DEV_MODE, EMAIL, MONTH, MONTH_STRING, NAME, NEXT_BUTTON_XPATH, SHORT_LOAD_TIME_SECONDS, SUBMIT_BUTTON_XPATH, TIME, TIME_BETWEEN_REFRESH_SECONDS, TIME_BUTTON_XPATH, TIME_WAIT_AFTER_CLICK, URL, LOAD_TIME_SECONDS, USER_DATA_DIR, YEAR
+
+options = webdriver.ChromeOptions()
+options.add_argument(USER_DATA_DIR)
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options)
+# driver.implicitly_wait(LOAD_TIME_SECONDS)
+wait = WebDriverWait(driver, LOAD_TIME_SECONDS)
 
 # Returns true if booking was successful, otherwise return false
 def make_booking():
     driver.get(URL)
     # Calendar_cells are found first to force webpage to be implicitly loaded, thereby updating the current_url before we check
+    wait.until(lambda _ : driver.find_elements(By.TAG_NAME, "td"))
     calendar_cells = driver.find_elements(By.TAG_NAME, "td")
 
     # Check month and year as specified in queryParam of url, updated from inputted url once page loads
     if(not MONTH_STRING in driver.current_url):
-        print(f"Error: Booking in {MONTH_STRING} not currently available")
-        return False
+        raise Exception(f"Booking in {MONTH_STRING} not currently available.")
 
     # Find day in calendar
-    button_to_press = None
-    for calendar_cell in calendar_cells:
-        calendar_button = calendar_cell.find_element(By.TAG_NAME, "button")
-        button_text = calendar_button.find_element(By.TAG_NAME, "span").text
-        if(not button_text):
-            continue
-        if(int(button_text) == DAY):
-            button_to_press = calendar_button
-            break
+    def get_calendar_button():
+        button_to_press = None
+        for calendar_cell in calendar_cells:
+            calendar_button = calendar_cell.find_element(By.TAG_NAME, "button")
+            button_text = calendar_button.find_element(By.TAG_NAME, "span").text
+            if(not button_text):
+                continue
+            if(int(button_text) == DAY):
+                button_to_press = calendar_button
+                break
+        if(not button_to_press):
+            raise TimeoutException
+        WebDriverWait(driver, SHORT_LOAD_TIME_SECONDS).until(lambda _ : button_to_press.is_enabled())
+        return button_to_press
 
-    if(not button_to_press or not button_to_press.is_enabled()):
-        print(f"Error: Booking in {str(DAY)} of {MONTH_STRING} not currently available")
-        return False
+    def get_next_button():
+        WebDriverWait(driver, SHORT_LOAD_TIME_SECONDS).until(lambda _ : driver.find_element(By.XPATH, TIME_BUTTON_XPATH))
+        time_button = driver.find_element(By.XPATH, TIME_BUTTON_XPATH)
+    
+        time_button.click()
+        time.sleep(TIME_WAIT_AFTER_CLICK)
+        WebDriverWait(driver, SHORT_LOAD_TIME_SECONDS).until(lambda _ : driver.find_element(By.XPATH, NEXT_BUTTON_XPATH))
+        next_button = driver.find_element(By.XPATH, NEXT_BUTTON_XPATH)
+        next_button.click()
+        time.sleep(TIME_WAIT_AFTER_CLICK)
+   
+    def input_details_and_confirm():
+        def send_keys_slow(inputElem, keys):
+            for c in keys:
+                inputElem.send_keys(c)
+                time.sleep(0.2)
+        WebDriverWait(driver, SHORT_LOAD_TIME_SECONDS).until(lambda _ : driver.find_element(By.TAG_NAME, "input"))
+        nameInput, emailInput, *_ = driver.find_elements(By.TAG_NAME, "input")
+        send_keys_slow(nameInput, NAME)
+        # nameInput.send_keys(NAME)
+        send_keys_slow(emailInput, EMAIL)
+        # emailInput.send_keys(EMAIL)
+        submitButton = driver.find_element(By.XPATH, SUBMIT_BUTTON_XPATH)
+        if(DEV_MODE):
+            # Stay at final page for debugging purposes
+            while(1):
+                pass
+        else:
+            submitButton.click()
+            while(1):
+                pass
 
+    # Wait for loading to finish, which will enable the button if there are slots
+        
+    try:
+        button_to_press = get_calendar_button()
+    except (TimeoutException, NoSuchElementException):
+        raise Exception(f"Booking in {str(DAY)} of {MONTH_STRING} not currently available.")
+        
     button_to_press.click()
-
-    # TODO: Input information
+    time.sleep(TIME_WAIT_AFTER_CLICK)
+    
+    try:
+        get_next_button()
+    except (TimeoutException, NoSuchElementException):
+        raise Exception(f"Booking of time {TIME} in {str(DAY)} of {MONTH_STRING} not currently available.")
+    
+    try:
+        input_details_and_confirm()
+    except (TimeoutException, NoSuchElementException):
+        raise Exception("Final stage of inputting name and email error. Likely caused by someone sniping the slot or the webpage changed")
     return True
 
 while(True):
-    if(make_booking()):
-        break
+    try:
+        if(make_booking()):
+            break
 
-    print(f"Waiting {TIME_BETWEEN_REFRESH_SECONDS} seconds before retry")
-    time.sleep(TIME_BETWEEN_REFRESH_SECONDS)
+        print(f"Waiting {TIME_BETWEEN_REFRESH_SECONDS} seconds before retry")
+        time.sleep(TIME_BETWEEN_REFRESH_SECONDS)
 
-print(f"Successfully booked {DAY}/{MONTH}/{YEAR}")
+    except Exception as e:
+        print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
+
+print(f"{Fore.GREEN}Successfully booked {DAY}/{MONTH}/{YEAR}{Style.RESET_ALL}")
